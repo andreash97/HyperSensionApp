f95918952c5e3dbe5f0ea55fddb385304be30665
Geranex <adrianantihack@hotmail.com>
2019-04-01 09:20:27 +0100

    we out here bois

--- app/src/main/java/com/example/hypersensionapp/MainActivity.kt
+++ app/src/main/java/com/example/hypersensionapp/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.hypersensionapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        connectbutton.setOnClickListener {
+            val intentconnect = Intent(this, ConnectedActivity::class.java)
+            startActivity(intentconnect)
         }
+
+
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
