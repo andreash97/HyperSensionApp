0d68876d1d506aa507e75be674e30ca585ead951
Andreas Helén <andreashelen26@gmail.com>
2019-04-08 12:42:53 +0200

    Implementert bluetooth kommunikasjon i appen. Ny aktvitet også.

--- app/src/main/java/com/example/hypersensionapp/MainActivity.kt
+++ app/src/main/java/com/example/hypersensionapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.hypersensionapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         connectbutton.setOnClickListener {
-            val intentconnect = Intent(this, ConnectedActivity::class.java)
+            val intentconnect = Intent(this, SelectDeviceActivity::class.java)
             startActivity(intentconnect)
         }
     }
---
+++ app/src/main/java/com/example/hypersensionapp/SelectDeviceActivity.kt
@@ -0,0 +1,95 @@
+package com.example.hypersensionapp
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.select_device_layout.*
+import org.jetbrains.anko.toast
+
+
+class SelectDeviceActivity : AppCompatActivity() {
+    var m_bluetoothAdapter: BluetoothAdapter? = null
+    lateinit var m_pairedDevices: Set<BluetoothDevice>
+    val REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        val EXTRA_ADRESS: String = "Device_address"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.select_device_layout)
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (m_bluetoothAdapter == null) {
+            toast("Enheten støtter ikke Bluetooth")
+            return
+            // Device doesn't support Bluetooth
+        }
+        if(!m_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+
+        select_device_refresh.setOnClickListener{ pairedDeviceList() }
+
+
+    }
+
+    private fun pairedDeviceList() {
+        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
+
+        if (m_pairedDevices.isEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
+                list.add(device)
+                Log.i("device", ""+device)
+            }
+        } else {
+            toast("Ingen parrede enheter funnet")
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if(resultCode == Activity.RESULT_OK) {
+                if(m_bluetoothAdapter!!.isEnabled) {
+                    toast("Bluetooth er aktivert")
+                }
+                else {
+                    toast("Bluetooth er deaktivert")
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                toast("Bluetooth aktivering har blitt kansellert")
+            }
+        }
+    }
+}
