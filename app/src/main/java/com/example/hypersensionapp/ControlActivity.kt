ae97d2cb24cb36dd6f27f2880d066188b9a5e758
Andreas Helén <andreashelen26@gmail.com>
2019-04-15 12:37:24 +0200

    Bluetooth imp.

---
+++ app/src/main/java/com/example/hypersensionapp/ControlActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hypersensionapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ControlActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/hypersensionapp/SelectDeviceActivity.kt
+++ app/src/main/java/com/example/hypersensionapp/SelectDeviceActivity.kt
@@ -4,25 +4,22 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.select_device_layout.*
 import org.jetbrains.anko.toast
 
-
 class SelectDeviceActivity : AppCompatActivity() {
-    var m_bluetoothAdapter: BluetoothAdapter? = null
-    lateinit var m_pairedDevices: Set<BluetoothDevice>
-    val REQUEST_ENABLE_BLUETOOTH = 1
+
+    private var m_bluetoothAdapter: BluetoothAdapter? = null
+    private lateinit var m_pairedDevices: Set<BluetoothDevice>
+    private val REQUEST_ENABLE_BLUETOOTH = 1
 
     companion object {
-        val EXTRA_ADRESS: String = "Device_address"
+        val EXTRA_ADDRESS: String = "Device_address"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +27,9 @@ class SelectDeviceActivity : AppCompatActivity() {
         setContentView(R.layout.select_device_layout)
 
         m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (m_bluetoothAdapter == null) {
+        if(m_bluetoothAdapter == null) {
             toast("Enheten støtter ikke Bluetooth")
             return
-            // Device doesn't support Bluetooth
         }
         if(!m_bluetoothAdapter!!.isEnabled) {
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
@@ -42,53 +38,44 @@ class SelectDeviceActivity : AppCompatActivity() {
 
         select_device_refresh.setOnClickListener{ pairedDeviceList() }
 
-
     }
 
     private fun pairedDeviceList() {
         m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
         val list : ArrayList<BluetoothDevice> = ArrayList()
 
-        if (m_pairedDevices.isEmpty()) {
+        if (!m_pairedDevices.isEmpty()) {
             for (device: BluetoothDevice in m_pairedDevices) {
-                list.add(device)
+                list.add(device.)
                 Log.i("device", ""+device)
             }
         } else {
             toast("Ingen parrede enheter funnet")
         }
 
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val device: BluetoothDevice = list[position]
+            val address: String = device.address
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+            val intent = Intent(this, ControlActivity::class.java)
+            intent.putExtra(EXTRA_ADDRESS, address)
+            startActivity(intent)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_ENABLE_BLUETOOTH) {
-            if(resultCode == Activity.RESULT_OK) {
-                if(m_bluetoothAdapter!!.isEnabled) {
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
                     toast("Bluetooth er aktivert")
-                }
-                else {
+                } else {
                     toast("Bluetooth er deaktivert")
                 }
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                toast("Bluetooth aktivering har blitt kansellert")
+                toast("Bluetooth aktivering har blitt avbrutt")
             }
         }
     }
