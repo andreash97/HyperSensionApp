dec2bb22581dde7ff74c644c321bd6ef6817405f
Geranex <adrianantihack@hotmail.com>
2019-05-03 13:05:21 +0200

    added 2 fragments

--- app/src/main/java/com/example/hypersensionapp/ControlActivity.kt
+++ app/src/main/java/com/example/hypersensionapp/ControlActivity.kt
@@ -9,16 +9,19 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.net.Uri
 import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.example.hypersensionapp.Fragments.testsignal
 import kotlinx.android.synthetic.main.connected.*
 import java.io.IOException
 import java.util.*
 
-class ControlActivity: AppCompatActivity() {
+class ControlActivity: AppCompatActivity(), testsignal.OnFragmentInteractionListener {
     companion object {
         var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         var m_bluetoothSocket: BluetoothSocket? = null
@@ -28,9 +31,11 @@ class ControlActivity: AppCompatActivity() {
         lateinit var m_address: String
 
     }
-    lateinit var notificationManager : NotificationManager
+
+    lateinit var testsignalfragment: testsignal
+    lateinit var notificationManager: NotificationManager
     lateinit var notificationChannel: NotificationChannel
-    lateinit var builder : Notification.Builder
+    lateinit var builder: Notification.Builder
     private var ChannelID = "com.example.hypersensionapp"
     private var description = "Low battery"
 
@@ -42,46 +47,60 @@ class ControlActivity: AppCompatActivity() {
         ConnectToDevice(this).execute()
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        testsignalbutton.setOnClickListener { // warning https://www.youtube.com/watch?v=Fo7WksYMlCU
-            val intent = Intent(this, ControlActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT)
-           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-               notificationChannel = NotificationChannel(ChannelID, description, NotificationManager.IMPORTANCE_HIGH)
-               notificationChannel.enableLights(true)
-               notificationChannel.lightColor = Color.YELLOW
-               notificationChannel.enableVibration(true)
-               notificationManager.createNotificationChannel(notificationChannel)
-               builder = Notification.Builder(this,ChannelID)
-                   .setContentTitle("HyperSension device LOW BATTERY")
-                   .setContentText("Your HyperSension device is low on battery, please charge")
-                   .setSmallIcon(R.mipmap.ic_launcher_round)
-                   .setLargeIcon((BitmapFactory.decodeResource(this.resources,R.mipmap.ic_launcher)))
-                   .setContentIntent(pendingIntent)
-                   .setAutoCancel(true)
-           } else {
-               builder = Notification.Builder(this)
-                   .setContentTitle("HyperSension device LOW BATTERY")
-                   .setContentText("Your HyperSension device is low on battery, please charge")
-                   .setSmallIcon(R.mipmap.ic_launcher_round)
-                   .setLargeIcon((BitmapFactory.decodeResource(this.resources,R.mipmap.ic_launcher)))
-                   .setContentIntent(pendingIntent)
-                   .setAutoCancel(true)
-           }
-            notificationManager.notify(1234,builder.build())
+        {
+            //testsignalbutton.setOnClickListener { // warning https://www.youtube.com/watch?v=Fo7WksYMlCU
+            //            val intent = Intent(this, ControlActivity::class.java)
+            //            val pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT)
+            //           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //               notificationChannel = NotificationChannel(ChannelID, description, NotificationManager.IMPORTANCE_HIGH)
+            //               notificationChannel.enableLights(true)
+            //               notificationChannel.lightColor = Color.YELLOW
+            //               notificationChannel.enableVibration(true)
+            //               notificationManager.createNotificationChannel(notificationChannel)
+            //               builder = Notification.Builder(this,ChannelID)
+            //                   .setContentTitle("HyperSension device LOW BATTERY")
+            //                   .setContentText("Your HyperSension device is low on battery, please charge")
+            //                   .setSmallIcon(R.mipmap.ic_launcher_round)
+            //                   .setLargeIcon((BitmapFactory.decodeResource(this.resources,R.mipmap.ic_launcher)))
+            //                   .setContentIntent(pendingIntent)
+            //                   .setAutoCancel(true)
+            //           } else {
+            //               builder = Notification.Builder(this)
+            //                   .setContentTitle("HyperSension device LOW BATTERY")
+            //                   .setContentText("Your HyperSension device is low on battery, please charge")
+            //                   .setSmallIcon(R.mipmap.ic_launcher_round)
+            //                   .setLargeIcon((BitmapFactory.decodeResource(this.resources,R.mipmap.ic_launcher)))
+            //                   .setContentIntent(pendingIntent)
+            //                   .setAutoCancel(true)
+            //           }
+            //            notificationManager.notify(1234,builder.build())
+        }
+        testsignalbutton.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, testsignalfragment)
+                .addToBackStack(testsignalfragment.toString())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
         }
         disconnectbutton.setOnClickListener {
             disconnect()
         }
+        testsignalfragment = testsignal.newInstance()
     }
+
     override fun onBackPressed() {
-        disconnect()
+        if(testsignalfragment.isVisible) {
+            super.onBackPressed()
+        } else
+            disconnect()
     }
 
     private fun sendCommand(input: String) {
         if (m_bluetoothSocket != null) {
-            try{
+            try {
                 m_bluetoothSocket!!.outputStream.write(input.toByteArray())
-            } catch(e: IOException) {
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
@@ -141,5 +160,10 @@ class ControlActivity: AppCompatActivity() {
             }
             m_progress.dismiss()
         }
+
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        Log.d("Info button", "info is comming")
     }
 }
--- app/src/main/java/com/example/hypersensionapp/MainActivity.kt
+++ app/src/main/java/com/example/hypersensionapp/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.example.hypersensionapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import com.example.hypersensionapp.Fragments.info
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), info.OnFragmentInteractionListener {
+
+    lateinit var infofragment:info
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        infobutton.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, infofragment)
+                .addToBackStack(infofragment.toString())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+        }
 
         connectbutton.setOnClickListener {
             val intentconnect = Intent(this, SelectDeviceActivity::class.java)
             startActivity(intentconnect)
         }
+
+
+        infofragment = info.newInstance()
+
     }
 
 
@@ -37,5 +56,9 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        Log.d("Info button", "info is comming")
     }
 }
